bf2bf0b625236a05b05943aecaa0db2da451e989
Babacar NDIAYE <babacar.ndiaye@idev-ic.net>
2021-02-21 14:20:49 +0000

    Generation Main class

---
+++ src/main/java/sn/Babs/Main.java
@@ -0,0 +1,7 @@
+package sn.Babs;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue sur la platforme de gestion de votre établissement scolaire!!");
+    }
+}
